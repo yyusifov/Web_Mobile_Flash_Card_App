7fb77fffa9d5344312b6185761d3951b5796b125
Yadigar Yusifov <yusifovyadigar1@gmail.com>
2023-12-18 23:46:01 +0400

    Certain changes were added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
-  const navigationBar = ()=>{
-    <div className='navBar'>
-      <div className='contForPages'>
-        <div className='page'>
-          <a><span>Home Page</span></a>
-        </div>
-        <div className='page'>
-          <a><span>Flash Card</span></a>
-        </div>
-        <div className='page'>
-          <a><span>Contact Me</span></a>
+  const navigationBar = () => {
+    return (
+      <div className='navBar'>
+        <div className='contForPages'>
+          <button className='page' onClick={() => moveToDiffPage('home')}>
+            <span>Home Page</span>
+          </button>
+          <button className='page' onClick={() => moveToDiffPage('flashCard')}>
+            <span>Flash Card</span>
+          </button>
+          <button className='page' onClick={() => moveToDiffPage('addCard')}>
+            <span>Add Card</span>
+          </button>
         </div>
       </div>
-    </div>
-  }
+    );
+  };
 
-  const addCard = ()=>{
-    <div>
+  const addCard = () => {
+    return (
       <div>
-        <span>Add a new card</span>
+        <div>
+          <span>Add a new card</span>
+        </div>
       </div>
-    </div>
-  }
-  const FlashCardPage = ()=>{
+    );
+  };
+
+  const FlashCardPage = () => {
     return (
       <div className='flashCardPage'>
         <div className='cardFrame'>
@@ -34,9 +38,7 @@ function App() {
             <span>Status</span>
           </div>
           <div className='question'>
-            <p>
-              Question
-            </p>
+            <p>Question</p>
           </div>
           <div className='dateCreated'>
             <span>Date created</span>
@@ -44,29 +46,43 @@ function App() {
         </div>
       </div>
     );
-  }
+  };
+
+  const [currentPage, showPage] = useState('home');
+
+  const moveToDiffPage = (page) => {
+    showPage(page);
+  };
+
   return (
     <div className="App">
-      <div>
-        <h1>Welcome to Flash Card Page</h1>
-      </div>
-      <div>
-        <p>
-          My name is Yadigar Yusifov. I am glad to accomodate you in my Flash Card Page.
-          I am senior Computer Sciences student at ADA University. 
-          I have done my internship in Div Academy as a back-end mentor.
-          I have experience in android development and front-end. 
-        </p>
-      </div>
-      <div>
-        <ul>
-          <li>Park Application: Link to repository: <a target='blank' href="https://github.com/yyusifov/Park_Application.git">Link</a></li>
-          <li>Online Bet Application: Link to repository: <a target='blank' href="https://github.com/yyusifov/Online-Bet-Application.git">Link</a></li>
-          <li>Quiz Application: Link to repository: <a target='blank' href="https://github.com/yyusifov/Quiz-App.git">Link</a></li>
-          <li>Portfolio: Link to repository: <a target='blank' href="https://github.com/yyusifov/Web-Mobile-Portfolio.git">Link</a></li>
-          <li>E-Store: Link to repository: <a target='blank' href="https://github.com/yyusifov/Web-Mobile-E-store.git">Link</a></li>
-        </ul>
-      </div>
+      {navigationBar()}
+
+      {currentPage === 'home' && (
+        <div>
+          <h1>Welcome to Flash Card Page</h1>
+          <div>
+            <p>
+              My name is Yadigar Yusifov. I am glad to accommodate you in my Flash Card Page.
+              I am a senior Computer Sciences student at ADA University.
+              I have done my internship at Div Academy as a back-end mentor.
+              I have experience in android development and front-end.
+            </p>
+          </div>
+          <div>
+            <ul>
+              <li>Park Application: Link to repository: <a target='blank' href="https://github.com/yyusifov/Park_Application.git">Link</a></li>
+              <li>Online Bet Application: Link to repository: <a target='blank' href="https://github.com/yyusifov/Online-Bet-Application.git">Link</a></li>
+              <li>Quiz Application: Link to repository: <a target='blank' href="https://github.com/yyusifov/Quiz-App.git">Link</a></li>
+              <li>Portfolio: Link to repository: <a target='blank' href="https://github.com/yyusifov/Web-Mobile-Portfolio.git">Link</a></li>
+              <li>E-Store: Link to repository: <a target='blank' href="https://github.com/yyusifov/Web-Mobile-E-store.git">Link</a></li>
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {currentPage === 'flashCard' && <FlashCardPage />}
+      {currentPage === 'addCard' && <>{addCard()}</>}
     </div>
   );
 }
