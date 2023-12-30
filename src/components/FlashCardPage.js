65f7844f1d664b657de6bf3980469420c6487ba8
Yadigar Yusifov <yusifovyadigar1@gmail.com>
2023-12-30 23:04:25 +0400

    update function for cards is configured

--- src/components/FlashCardPage.js
+++ src/components/FlashCardPage.js
@@ -2,6 +2,73 @@ import React, { useEffect, useState } from 'react';
 import "./FlashCardPage.css"
 
 
+
+const editCard = (card) => {
+
+  const countryName = document.getElementById(`countryName_${card.id}`);
+
+  const questionForCard = document.getElementById(`question_${card.id}`);
+
+  if(document.getElementById(`editButton_${card.id}`).textContent === "Edit"){
+
+    document.getElementById(`editButton_${card.id}`).textContent = "Save";
+
+    document.getElementById(`countryName_${card.id}`).contentEditable = true;
+
+    document.getElementById(`question_${card.id}`).contentEditable = true;
+  }
+  else{
+    document.getElementById(`editButton_${card.id}`).textContent = "Edit";
+
+    document.getElementById(`countryName_${card.id}`).contentEditable = false;
+
+    document.getElementById(`question_${card.id}`).contentEditable = false;
+
+    const updatedData = {
+      id: card.id,
+      countryName: countryName,
+      flag: `/countryFlags/Flag_of_${card.countryName}.svg`,
+      question:questionForCard,
+      capital: card.capital
+    };
+
+    updateOnFlashCardPage(updatedData)
+  }
+};
+
+const updateOnFlashCardPage = (card) => {
+  const countryName = document.getElementById(`countryName_${card.id}`).textContent;
+  const questionForCard = document.getElementById(`question_${card.id}`).textContent;
+
+  const updatedData = {
+    id: card.id,
+    countryName: countryName,
+    flag: `/countryFlags/Flag_of_${card.countryName}.svg`,
+    question: questionForCard,
+    capital: card.capital
+  };
+
+  fetch(`http://localhost:3000/cards/${updatedData.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedData),
+  })
+    .then(response => response.json())
+    .then(updatedDataFromServer => {
+      console.log('Data updated on the server:', updatedDataFromServer);
+    })
+    .catch(error => console.error('Error updating data on the server:', error))
+};
+/*
+const deleteCard = (cardId, cardInfo) => {
+  const isolatedListFromPreviousElement = cardInfo.filter((card) => {card.id !== cardId})
+  
+
+}
+*/
+
 const FlashCardPage = () => {
 
     const [cardInfo, setCardInfo] = useState([]);
@@ -48,29 +115,27 @@ const FlashCardPage = () => {
       </div>
         <div className='flashCardRow'>
           {cardInfo.map(card => (
-            <div className='cardFrame'>
+            <div className='cardFrame' id='cardFrame'>
               <div className='status'>
                 <span>Status</span>
               </div>
               <div className="countryFlag">
                 <img className='flag' src={card.flag} alt="Country Flag"/>
               </div>
-              <div className="countryName">
+              <div className="countryName" id={`countryName_${card.id}`}>
                 <span>{card.countryName}</span>
               </div>
-              <div className='question'>
-                <p>What is the capital of {card.countryName}</p>
+              <div className='question' id={`question_${card.id}`}>
+                <p>{card.question}</p>
               </div>
               <div className='dateCreated'>
                 <span>Date created</span>
               </div>
-              <div className='modificationOnCard'>
-                <button>
-                  Edit
-                </button>
-                <button>
-                  Delete
-                </button>
+              <div className='modificationOnCard' id={`modificationOnCard_${card.id}`}>
+                <button id={`editButton_${card.id}`} onClick={()=>{editCard(card)}}>Edit</button>
+                <button>Delete</button>
+
+                {/*<button onClick={() => {deleteCard(card.id, cardInfo)}}>Delete</button>*/}
               </div>
             </div>
           ))}
